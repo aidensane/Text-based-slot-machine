6adf26ef15aaa7fb1ef6903d1422c8ef35b77c7f
aidensane <98586059+aidensane@users.noreply.github.com>
2023-01-03 13:08:02 +0100

    Getting number of lines

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit?: $")
@@ -13,8 +18,24 @@ def deposit():
     return amount
 
 
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines you'd like to deposit (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a number.")
+
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
 
 
 main()
